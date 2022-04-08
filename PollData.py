43296bf06945caf33f459aea781c8131cbca79c2
dvidal <vidal.damien@gmail.com>
2022-04-08 22:44:51 +0100

    1.0

--- PollData.py
+++ PollData.py
@@ -23,6 +23,7 @@ class PollData:
 
     """
     def load(self, source: str):
+
         with urllib.request.urlopen(source) as s:
             self.Data = json.load(s)
         self.PInstitute = set([x['nom_institut'] for x in self.Data])
@@ -33,11 +34,11 @@ class PollData:
                         if candidat['candidat'] not in self.Candidates:
                             self.Candidates.add(candidat['candidat'])
 
-    def formatdata(self, tour: str):
+    def formatdata(self, tour: str, hypothese: str):
         self.FormattedData = dict(zip([x[0] for x in self.Settings['Candidats']], [[] for _ in self.Settings['Candidats']]))
         for sondage in [x for x in self.Data if (x['nom_institut'] in self.Settings['Instituts'])]:
             for t in [x for x in sondage['tours'] if x['tour'] == tour]:
-                for hypo in t['hypotheses']:
+                for hypo in [x for x in t['hypotheses'] if hypothese == '*' or hypothese == x['hypothese']]:
                     for result in [x for x in hypo['candidats'] if x['candidat'] in self.FormattedData.keys()]:
                         self.FormattedData[result['candidat']].append({'candidat': result['candidat'],
                                                                        'date': date.fromisoformat(sondage['fin_enquete']),
@@ -75,7 +76,7 @@ class PollData:
         fig, ax = plt.subplots(nrows=1, ncols=1)
         tf = {'family': 'serif', 'color':  'white', 'weight': 'normal'}
         fig.suptitle('Intentions de vote à l\'élection présidentielle de 2022', fontsize=22, fontdict=tf)
-        ax.set_title('Instituts pris en compte : '+', '.join(foo.Settings['Instituts']), fontsize=10, color='white')
+        ax.set_title('Instituts pris en compte : '+', '.join(self.Settings['Instituts']), fontsize=10, color='white')
         for candidat in candidatA:
             couleur = [x[1] for x in self.Settings['Candidats'] if x[0] == candidat].pop() +\
                       [x[2] for x in self.Settings['Candidats'] if x[0] == candidat].pop()
@@ -87,42 +88,33 @@ class PollData:
             KF = KalmanFilter(data[0]['score'])
             dates, scores, Kscores = [x['date'] for x in data], [x['score'] for x in data],\
                                      [KF.filter(x['score']) for x in data]
+            fdates = []
+            fKscores = []
+            acc = (dates[0], Kscores[0])
+            for i in range(len(dates)):
+                if dates[i] > acc[0]:
+                    fdates.append(acc[0])
+                    fKscores.append(acc[1])
 
+                acc = (dates[i], Kscores[i])
+
+            fdates.append(acc[0])
+            fKscores.append(acc[1])
             ax.plot_date(dates, scores, color=couleur)
 
-            ax.plot_date(dates, [x[0][0] for x in Kscores], color=ckline, ls='-', marker=None)
-            ax.annotate('   '+str(round(Kscores[-1][0][0], 0)) + '% - '+candidat, (dates[-1], Kscores[-1][0][0]),
+            ax.plot_date(fdates, [x[0][0] for x in fKscores], color=ckline, ls='-', marker=None)
+            ax.annotate('   '+str(round(fKscores[-1][0][0], 1)) + '% - '+candidat, (dates[-1], Kscores[-1][0][0]),
                         color=ckline)
 
             # Marges d'erreur
-            plt.fill_between(dates, [x[2][0] for x in Kscores], [x[3][0] for x in Kscores], color=cerror)
+            plt.fill_between(fdates, [x[2][0] for x in fKscores], [x[3][0] for x in fKscores], color=cerror)
 
         self.setchartsettings(fig, ax)
         plt.show()
 
-## TODO : Replace with proper script / argsparse
+
+# TODO : Replace with proper script
+
+
 if __name__ == '__main__':
-    foo = PollData()
-    foo.Settings['Instituts'] = {'Opinion Way', 'Odoxa', 'Ifop', 'Ipsos',
-                                 'Harris Interactive', 'BVA', 'Elabe', 'Kantar Public'}
-
-    # foo.Settings['Instituts'] = {'Elabe'}
-
-
-    foo.Settings['Candidats'] = {('Jean Lassalle', '#26c4ec', '30'), ('Emmanuel Macron', '#ffeb00', '30'),
-                                 ('Valérie Pécresse', '#007FFF', '50'), ('Nathalie Arthaud', '#bb0000', '30'),
-                                 ('Fabien Roussel', '#dd0000', '30'), ('Nicolas Dupont-Aignan', '#0082C4', '40'),
-                                 ('Philippe Poutou', '#bb0000', '30'), ('Anne Hidalgo', '#FF8080', '40'),
-                                 ('Jean-Luc Mélenchon', '#FF8080', '40'), ('Eric Zemmour', '#909090', '50'),
-                                 ('Yannick Jadot', '#00c000', '30'), ('Marine Le Pen', '#A775FF', '40')}
-    # Base codes couleurs : https://fr.wikipedia.org/wiki/Mod%C3%A8le:Infobox_Parti_politique_fran%C3%A7ais/couleurs
-    # Le canal alpha est utilisé pour assurer la visibilité, en particulier pour les partis de droite.
-    # Exception : gris de Zemmour, bleu de Pécresse, éclaircis pour être mieux visible.
-    # Teinte Le Pen modifiée même raison.
-
-    foo.Settings['From'] = date(2022, 1, 1)
-    foo.Settings['To'] = date(2099, 1, 1)
-    foo.load('https://raw.githubusercontent.com/nsppolls/nsppolls/master/presidentielle.json')
-    foo.formatdata('Premier tour')
-
-    foo.getimage(['Marine Le Pen', 'Jean-Luc Mélenchon'])
+    pass
--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+from PollData import PollData
+from datetime import date
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+foo = PollData()
+foo.Settings['Instituts'] = {'Opinion Way', 'Odoxa', 'Ifop', 'Ipsos',
+                             'Harris Interactive', 'BVA', 'Elabe', 'Kantar Public'}
 
+foo.Settings['Candidats'] = {('Jean Lassalle', '#26c4ec', '30'), ('Emmanuel Macron', '#ffeb00', '30'),
+                             ('Valérie Pécresse', '#007FFF', '50'), ('Nathalie Arthaud', '#bb0000', '30'),
+                             ('Fabien Roussel', '#dd0000', '30'), ('Nicolas Dupont-Aignan', '#0082C4', '40'),
+                             ('Philippe Poutou', '#bb0000', '30'), ('Anne Hidalgo', '#FF8080', '40'),
+                             ('Jean-Luc Mélenchon', '#FF8080', '40'), ('Eric Zemmour', '#909090', '50'),
+                             ('Yannick Jadot', '#00c000', '30'), ('Marine Le Pen', '#A775FF', '40')}
+    # Base codes couleurs : https://fr.wikipedia.org/wiki/Mod%C3%A8le:Infobox_Parti_politique_fran%C3%A7ais/couleurs
+    # Le canal alpha est utilisé pour assurer la visibilité, en particulier pour les partis de droite.
+    # Exception : gris de Zemmour, bleu de Pécresse, éclaircis pour être mieux visible.
+    # Teinte Le Pen modifiée même raison.
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+foo.Settings['From'] = date(2022, 1, 1)
+foo.Settings['To'] = date(2099, 1, 1)
+foo.Settings['Source'] = 'https://raw.githubusercontent.com/nsppolls/nsppolls/master/presidentielle.json'
 
+foo.load(foo.Settings['Source'])
+foo.formatdata('Premier tour', '*')
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+foo.getimage(['Emmanuel Macron', 'Marine Le Pen', 'Jean-Luc Mélenchon', 'Valérie Pécresse', 'Eric Zemmour',
+              'Fabien Roussel', 'Yannick Jadot', 'Jean Lassalle', 'Nathalie Arthaud', 'Nicolas Dupont-Aignan',
+              'Philippe Poutou', 'Anne Hidalgo'])
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+foo.formatdata('Deuxième tour', 'Hypothèse Macron / Le Pen')
+
+foo.getimage(['Emmanuel Macron', 'Marine Le Pen'])
